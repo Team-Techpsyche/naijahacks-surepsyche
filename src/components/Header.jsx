457bf9e449ba7f7209d63c946c9aea31603d4d47
obayomi96 <obayomimartins96@gmail.com>
2019-12-15 12:44:19 +0100

    Landing page done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom';
 const Header = () => {
 	return (
 		<header>
-			<div>
-					<ul>
-						<li><Link className="active" href="">surepsyches</Link></li>
-						<li><Link to="">About us</Link></li>
-						<li><Link to="">FAQ</Link></li>
-					</ul>
-					<div className="auth">
-						<p><Link className="sign-in" to="">Sign in</Link></p>
-						<button>Get Started</button>
-					</div>
-        </div>
+			<div className="header-div">
+				<ul>
+					<li><Link className="active logo" to="">SUREP<span style={{color: "green"}}>SYCHE</span></Link></li>
+					<li><Link className="other-links" to="">About us</Link></li>
+					<li><Link className="other-links" to="">FAQ</Link></li>
+				</ul>
+				<div className="auth">
+					<p><Link className="sign-in" to="">Sign in</Link></p>
+					<button>Get Started</button>
+				</div>
+      </div>
     </header>
 	);
 }
