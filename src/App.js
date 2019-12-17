abedd966b9e3ad185ab376a3741e05046a68a816
obayomi96 <obayomimartins96@gmail.com>
2019-12-17 16:01:26 +0100

    Service workers page done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from './components/LandingPage';
+import SocialWorker from './components/SocialWorkers';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <BrowserRouter>
-         <LandingPage />
-        </BrowserRouter>
-      </div>
-    )
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/social-workers" component={SocialWorker} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
-}
+};
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
 				<ul>
 					<li><Link className="active logo" to="">SUREP<span style={{color: "green"}}>SYCHE</span></Link></li>
 					<li><Link className="other-links" to="">About us</Link></li>
-					<li><Link className="other-links" to="">FAQ</Link></li>
+					<li><Link className="other-links" to="/social-workers">Contact Specialists</Link></li>
 				</ul>
 				<div className="auth">
 					<p><Link className="sign-in" to="">Sign in</Link></p>
